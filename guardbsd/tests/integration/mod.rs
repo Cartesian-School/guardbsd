abf9f1034286d25769ccdbb6a3e43de19763e58d
Siergej Sobolewski <s.sobolewski@hotmail.com>
2025-12-02 22:59:17 +0100

    Delete guardbsd directory

--- guardbsd/tests/integration/mod.rs
+++
@@ -1,6 +0,0 @@
-// tests/integration/mod.rs
-// Integration Test Suite Entry Point
-// ============================================================================
-
-mod ipc_integration;
-mod capability_integration;
