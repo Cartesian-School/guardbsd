407c113479e7c7357e94bfdbe21daa29c6b2f1c3
Siergej Sobolewski <s.sobolewski@hotmail.com>
2026-03-09 08:31:35 +0100

    Delete tests directory

--- tests/integration/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Project: GuardBSD Winter Saga version 1.0.0
-//! Package: tests_integration
-//! Copyright © 2025 Cartesian School. Developed by Siergej Sobolewski.
-//! License: BSD-3-Clause
-//!
-//! Wejście zestawu testów integracyjnych.
-
-mod capability_integration;
-mod ipc_integration;
