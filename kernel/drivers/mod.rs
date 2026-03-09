64baed974dbafe0984bf214139b757dfe61f975d
Siergej Sobolewski <s.sobolewski@hotmail.com>
2026-03-09 08:29:46 +0100

    Delete kernel directory

--- kernel/drivers/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Project: GuardBSD Winter Saga version 1.0.0
-//! Package: kernel_drivers
-//! Copyright © 2025 Cartesian School. Developed by Siergej Sobolewski.
-//! License: BSD-3-Clause
-//!
-//! Moduł sterowników urządzeń (klawiatura, konsola).
-
-pub mod keyboard;
-pub mod console;
