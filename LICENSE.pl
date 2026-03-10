43cd2f92abeaa94b25141ca64ae51c7d7cf6f715
Siergej Sobolewski <s.sobolewski@hotmail.com>
2026-03-10 18:02:08 +0100

    Add LICENSE.pl with copyright and usage terms
    Added a license file outlining copyright and usage terms.

---
+++ LICENSE.pl
@@ -0,0 +1,15 @@
+Copyright (c) 2025–2026  Cartesian School Siergej Sobolewski. Wszelkie prawa zastrzeżone.
+
+OPROGRAMOWANIE PRAWNIE ZASTRZEŻONE I POUFNE
+
+Niniejsze oprogramowanie oraz towarzyszące mu materiały („Oprogramowanie”) stanowią własność poufną Cartesian School i są chronione prawem autorskim, tajemnicą przedsiębiorstwa oraz innymi przepisami dotyczącymi własności intelektualnej.
+
+Żadna część Oprogramowania nie może być kopiowana, modyfikowana, rozpowszechniana, poddawana inżynierii odwrotnej, dekompilowana ani wykorzystywana w jakikolwiek sposób, z wyjątkiem przypadków określonych w ważnej, pisemnej umowie licencyjnej komercyjnej podpisanej przez upoważnionego przedstawiciela Cartesian School.
+
+Jakiekolwiek nieuprawnione użycie, reprodukcja, modyfikacja lub dystrybucja Oprogramowania jest surowo zabroniona i może skutkować odpowiedzialnością cywilną oraz karną.
+
+OPROGRAMOWANIE JEST UDOSTĘPNIANE „TAK JAK JEST”, BEZ JAKIEJKOLWIEK GWARANCJI.
+
+Wszelkie prawa zastrzeżone.
+
+GuardBSD® jest zarejestrowanym znakiem towarowym Cartesian School.
